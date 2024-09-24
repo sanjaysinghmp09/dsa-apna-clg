44d3042b14407f907ec6bd2431edbfe4ca365591
Sanjay Singh <95602346+sanjaysinghmp09@users.noreply.github.com>
2024-09-25 01:09:20 +0530

    Create 1st.cpp

---
+++ 1st.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ;
+
+// This is starting 
+
+int main () {
+    int age = 29 ;
+    char grade = 'a' ;
+    float PI = 3.14 ;
+
+    bool isSafe = true ;
+    cout << "Sanjay Singh" << endl ;
+    int userAge = 19 ;
+    cout << PI << endl ;
+    cout << isSafe << endl ;
+     cout << age << endl ;
+    return 0 ;
+}
